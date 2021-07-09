305e6c3c5f8e6ee5dde24b570b0206cb34d58b25
anthony rodriguez <anthony@arod.la>
2021-07-09 14:33:30 -0700

    Removed Nav Options Dropdown

--- src/Components/NavComponent.js
+++ src/Components/NavComponent.js
@@ -32,23 +32,7 @@ const Example = (props) => {
             <NavItem>
               <NavLink href="https://github.com/Arods642">GitHub/Arods642</NavLink>
             </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+
           </Nav>
          </Collapse>
       </Navbar>
